121ab570b89de47ee587105e58ccba247f9320cb
Pranay <pranaypatil6575@gmail.com>
2023-07-19 12:42:05 +0530

    Added Data Validation using express-validator

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-
-
-
 const UserSchema = new Schema({
     name:{
         type: String,
@@ -24,4 +21,6 @@ const UserSchema = new Schema({
     }
   });
 
-  module.exports = mongoose.model('user', UserSchema)
+  const User = mongoose.model('user', UserSchema);
+  User.createIndexes()
+  module.exports = User
