3e5a687fd4d60650714ba32b70af54b2d021c17e
Pranay <pranaypatil6575@gmail.com>
2023-07-27 03:01:37 +0530

    Login endpoint created and send auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,21 +7,19 @@ const jwt = require('jsonwebtoken');
 
 const JWT_secret = 'Thisisjwttoken@thing';
 // Create a User using POST "/api/auth/createuser"
-router.post("/", [
+router.post("/createuser", [
   body('name', "Enter a valid Name").isLength({ min: 3 }),
   body('email', "Enter a valid Email").isEmail(),
   body('password', "Valid password should be more than 5 characters ").isLength({ min: 5 })
 ], async (req, res) => {
   // If there are errors , return Bad request and the errors
   const errors = validationResult(req);
-  console.log(req.body);
-
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   } else {
     // Check wheather the user with same email exists already
     try {
-      // Created new user
+
       let user = await User.findOne({ email: req.body.email });
 
       if (user) {
@@ -29,28 +27,67 @@ router.post("/", [
       }
       const salt = await bcrypt.genSalt(10);
       const secPass = await bcrypt.hash(req.body.password, salt);
+      // Created new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: secPass,
       })
       const data = {
-        user :{
+        user: {
           id: user.id
         }
       }
-     const authToken =  jwt.sign(data, JWT_secret);
+      const authToken = jwt.sign(data, JWT_secret);
 
-     res.json({authToken})
-     
+      res.json({ authToken })
+
       // res.json(user)
     }
     catch (err) {
-
       res.status(500).send('Error has occured')
+    }
+  }
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Authenticate a User using POST "/api/auth/login", no login req.
+router.post('/login', [
+  body('email', "Enter a valid Email").isEmail(),
+  body('password', "Password shouldn't be blank ").exists()
+], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  else {
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({email})
+      if (!user) {
+        return res.status(400).json({ error: "Please try to login with correct credentials !" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password)
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Please try to login with correct credentials !" })
+      }
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+
+      const authToken = jwt.sign(data, JWT_secret);
+      res.json({ authToken });
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send({ error: "Internal sever error occur" })
     }
   }
 
 })
-
 module.exports = router;
