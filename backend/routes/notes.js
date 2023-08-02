b92cf336abb820bfcb57b6f32aaadcbdcb989189
Pranay <pranaypatil6575@gmail.com>
2023-08-02 13:33:19 +0530

    new notes routes created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router =   express.Router();
+var fetchuser = require("../middleware/fetchuser");
+const Notes = require('../models/Notes');
 
-
-router.get("/", (req, res) =>{
-
-    res.json([])
+// ROUTE-1 Get all notes using GET "/api/notes/fetchAllnotes"
+router.get("/fetchAllnotes", fetchuser, async (req, res) =>{
+    try{
+        const notes = await Notes.find({user: req.user.id});
+        res.json(notes);
+    }catch(error){
+        res.status(500).send("Error has occured");
+    }
 })
 
 module.exports = router;
