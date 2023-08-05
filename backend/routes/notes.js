65a7f2e6251baeaefc37be60bab579fb6ce7c476
Pranay <pranaypatil6575@gmail.com>
2023-08-05 20:45:08 +0530

    Update note route done!

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -58,6 +58,17 @@ const{title, description, tag} = req.body;
      if(description){newNote.description = description};
      if(tag){newNote.tag = tag};
 
+     // Find the note to be updated and update it
+     let note = await Note.findById(req.params.id);
+     if(!note){return res.status(404).send("Data Not Found")};
+
+     if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Unauthorized Entry");
+     }
+
+     note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+     res.json({note});
+
       });
 
 module.exports = router;
